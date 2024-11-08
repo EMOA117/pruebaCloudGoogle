a4aa24e2b0fa670be3e9497849f253866f124b39
EMOA117 <emontesdeocaa1900@alumno.ipn.mx>
2024-11-07 22:28:02 -0600

    ejemplo de uso googlecloud

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 let express = require('express')
 
-//cloud storage connect to
-
-const {Storage} = require('@google-cloud/storage')
+// Cloud storage connection
+const { Storage } = require('@google-cloud/storage')
 
 const storage = new Storage({
     keyFilename: 'key.json'
@@ -13,7 +12,7 @@ let multer = require('multer')
 let app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 const upload = multer({
     storage: multer.memoryStorage()
@@ -24,17 +23,16 @@ const bucket = storage.bucket(bucketName)
 
 app.set('view engine', 'ejs')
 
-
 app.post('/upload', upload.single('file'), async (req, res) => {
     const file = req.file
-    if(!file){
+    if (!file) {
         res.status(400).send('Please upload a file')
         return
     }
 
     const fileName = Date.now() + '-' + file.originalname
 
-    //convert to blob
+    // Convert to blob
     const blob = bucket.file(fileName)
     const blobStream = blob.createWriteStream({
         metadata: {
@@ -51,23 +49,57 @@ app.post('/upload', upload.single('file'), async (req, res) => {
     })
 
     blobStream.end(file.buffer)
-
-
 })
 
-
-app.get('/', (req, res) => {
-    //read the files in the bucket
-
+app.get('/', async (req, res) => {
     try {
-        const [files] = bucket.getFiles()
-
-        res.render('index', {files})
-    }catch (error) {
-        res.status(500).send
+        // Read the files in the bucket
+        const [files] = await bucket.getFiles()
+        res.render('index', { files })
+    } catch (error) {
+        res.status(500).send('Error fetching files from the bucket: ' + error.message)
     }
 })
 
+//download
+app.get('/download/:file', async (req, res) => {
+    try {
+        const file = bucket.file(req.params.file)
+        const fileExists = await file.exists()
+
+        if (!fileExists[0]) {
+            res.status(404).send('File not found')
+            return
+        }
+
+        const signedUrl = await file.getSignedUrl({
+            action: 'read',
+            expires: Date.now() + 1000 * 60 * 5
+        })
+
+        res.redirect(signedUrl)
+
+        const readStream = file.createReadStream()
+        readStream.pipe(res)
+    } catch (error) {
+        res.status(500).send('Error downloading the file: ' + error.message)
+    }
+})
+
+app.post('/delete', async (req, res) => {
+    const filename = req.body.fileName
+    if (!filename) {
+        res.status(404).send('File not found')
+        return
+    }
+    try {
+        await bucket.file(filename).delete()
+        res.redirect('/')
+    } catch (error) {
+        console.log(error)
+    }
+}) 
+
 app.listen(4000, () => {
-    console.log('App is running in 4000')
+    console.log('App is running on port 4000')
 })
